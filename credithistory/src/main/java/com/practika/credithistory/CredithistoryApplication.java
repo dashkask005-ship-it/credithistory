f4b1b59e923e95f803e181259d23147fb150a832
britnisc <britnisc@student.21-school.ru>
2026-07-01 20:58:23 +0300

    Создание проекта

---
+++ credithistory/src/main/java/com/practika/credithistory/CredithistoryApplication.java
@@ -0,0 +1,13 @@
+package com.practika.credithistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CredithistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CredithistoryApplication.class, args);
+	}
+
+}
---
+++ credithistory/src/test/java/com/practika/credithistory/CredithistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practika.credithistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CredithistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
